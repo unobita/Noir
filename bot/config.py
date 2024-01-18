227eb3db5c1eb6ad238be631d352e97fe20def1d
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2024-01-18 14:48:00 +0530

    config.py

--- bot/config.py
+++ bot/config.py
@@ -9,7 +9,7 @@ class Config(object):
     LOG_CHANNEL = get_config("ABotLogs")
     UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
      # Get these values from my.telegram.org
-    AUTH_USERS = {6748415360}
+    AUTH_USERS = {2036803347}
 # array , simplest method was AUTH_USERS = [] ; AUTH_USERS.append(your telegram id) 🤣
     # array to store the channel ID who are authorized to use the bot
     # dont u fucking remove this id 😤
