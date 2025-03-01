b76629665863c6cb93e864ab0be365d0c6223542
unobita <unobita3@gmail.com>
2025-03-01 21:56:02 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,15 +6,15 @@ class Config(object):
     # sucks Dude
     APP_ID = int(get_config("APP_ID", "26376042"))
     API_HASH = get_config("API_HASH", "1f5343b0646645ca1eaf7c4759fc248f")
-    LOG_CHANNEL = get_config("LOG_CHANNEL", "ABotLogs")
+    LOG_CHANNEL = get_config("LOG_CHANNEL", "ygsrhggd")
     UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
      # Get these values from my.telegram.org
-    AUTH_USERS = get_config("AUTH_USERS", "-1002103913042,2036803347,6691641006")
+    AUTH_USERS = get_config("AUTH_USERS", "5385471287")
     AUTH_USERS = [int(user_id) for user_id in AUTH_USERS.split(',')]
     #simplest method was AUTH_USERS = [] ; AUTH_USERS.append(your telegram id) 🤣
     # array to store the channel ID who are authorized to use the bot
     # dont u fucking remove this id 😤
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "6716504661:AAHIxJpkdnapwwAkolQFeoScIUcCzsUCWfw")
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "5822396703:AAG7sX8J2gaYpF9J_ZHGphpqZ4VXSdpF")
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     # Telegram maximum file upload size
